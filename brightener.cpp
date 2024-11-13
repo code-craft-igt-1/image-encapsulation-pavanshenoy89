f4a47c3df667c1e9d608ae9087d920db053f4ce4
pavanshenoy89 <71321360+pavanshenoy89@users.noreply.github.com>
2024-11-13 21:31:06 +0530

    Add files via upload

--- brightener.cpp
+++ brightener.cpp
@@ -6,20 +6,20 @@ ImageBrightener::ImageBrightener(std::shared_ptr<Image> inputImage)
 }
 
 int ImageBrightener::BrightenWholeImage() {
-    // For brightening, we add a certain grayscale (25) to every pixel.
-    // While brightening, some pixels may cross the max brightness. They are
-    // called 'attenuated' pixels
     int attenuatedPixelCount = 0;
     int rows = m_inputImage->GetRows();
     int columns = m_inputImage->GetColumns();
+    const int maxBrightness = 255;
+    const int brightnessIncrement = 25;
+
     for (int x = 0; x < rows; x++) {
         for (int y = 0; y < columns; y++) {
-            int pixel_value = m_inputImage->GetPixel(x, y);
-            if (pixel_value > (255 - 25)) {
+            int pixelValue = m_inputImage->GetPixel(x, y);
+            if (pixelValue > (maxBrightness - brightnessIncrement)) {
                 ++attenuatedPixelCount;
-                m_inputImage->SetPixel(x, y, 255);
+                m_inputImage->SetPixel(x, y, maxBrightness);
             } else {
-                m_inputImage->SetPixel(x, y, pixel_value + 25);
+                m_inputImage->SetPixel(x, y, pixelValue + brightnessIncrement);
             }
         }
     }
