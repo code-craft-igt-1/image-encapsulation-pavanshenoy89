6c0022a3af55a90c7a86b1f382dc2d7978f7cc19
pavanshenoy89 <71321360+pavanshenoy89@users.noreply.github.com>
2024-11-13 22:02:46 +0530

    Add files via upload

--- brightener.cpp
+++ brightener.cpp
@@ -7,22 +7,24 @@ ImageBrightener::ImageBrightener(std::shared_ptr<Image> inputImage)
 
 int ImageBrightener::BrightenWholeImage() {
     int attenuatedPixelCount = 0;
-    int rows = m_inputImage->GetRows();
-    int columns = m_inputImage->GetColumns();
-    const int maxBrightness = 255;
-    const int brightnessIncrement = 25;
 
-    for (int x = 0; x < rows; x++) {
-        for (int y = 0; y < columns; y++) {
-            int pixelValue = m_inputImage->GetPixel(x, y);
-            int updatedPixelValue = pixelValue + brightnessIncrement;       
-            if (updatedPixelValue > maxBrightness) {
-                ++attenuatedPixelCount;
-                updatedPixelValue = maxBrightness;
-            }         
+    for (int x = 0; x < m_inputImage->GetRows(); x++) {
+        for (int y = 0; y < m_inputImage->GetColumns(); y++) {
+            int updatedPixelValue = IncreaseBrightness(m_inputImage->GetPixel(x, y), attenuatedPixelCount);
             m_inputImage->SetPixel(x, y, updatedPixelValue);
         }
     }
 
     return attenuatedPixelCount;
+}
+
+int ImageBrightener::IncreaseBrightness(int pixelValue, int& attenuatedPixelCount) {
+    const int maxBrightness = 255;
+    const int brightnessIncrement = 25;
+    int updatedPixelValue = pixelValue + brightnessIncrement;
+    if (updatedPixelValue > maxBrightness) {
+        ++attenuatedPixelCount;
+        updatedPixelValue = maxBrightness;
+    }
+    return updatedPixelValue;
 }
